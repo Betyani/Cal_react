9f493274b71b131401254729643a82f7a950ee45
betyani <dhwhdgns777@gmail.com>
2025-07-09 16:21:07 +0900

    initial commint

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import ProductRegister from './prodcut/ProductRegister';
+
+function App() {
+    return (
+        <>
+        <ProductRegister />
+        </>
+    );
+}
+export default App;
