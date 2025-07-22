96b2b1a12b2d45b0657773caebefbb88b6084b18
heyiyou <billboy454@gmail.com>
2025-07-22 12:05:08 +0900

    2차 게시판수정

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import Login from './pages/Login';
+import Register from './pages/Register';
 import BoardDetail from './components/board/BoardDetail';
 import BoardForm from './components/board/BoardForm';
-import BoardList from './components/board/BoardList';
+import BoardList from './components/Board/BoardList';
 import ProductDelete from './components/ProductDelete/ProductDelete';
 import ProductDetail from './components/ProductDetail/ProductDetail';
 import ProductForm from './components/ProductForm/ProductForm';
 import ProductList from './components/ProductList/ProductList';
 import ProductRegister from './product/ProductRegister';
 import Home from './components/Home/Home'; // 접속시 나오는 홈페이지; 이러면 관리가 명확해진하여 도전해봤습니다.
-
+import ProductEdit from './components/ProductEdit';
 function App() {
   return (
     <Router>
       <div className="App">
+        <Header /> {/*  로그인 상태, 로그아웃 관리 */}
         {/* <h1> 상품 받아라 </h1> */}
         <Routes>
           <Route path="/" element={<Home />} /> {/* 여기를 Home으로 바꿈 */}
@@ -25,7 +29,8 @@ function App() {
           <Route path="/board/detail/:id" element={<BoardDetail />} />
           <Route path="/board/form" element={<BoardForm />} />
           <Route path="/board/form/:id" element={<BoardForm />} />
-
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
           {/* /board/list 를 디폴트로 하는거 */}
           {/* <Route path="*" element={<Navigate replace to="/board/list" />} /> */}
         </Routes>
@@ -35,6 +40,7 @@ function App() {
           <ProductRegister />
           <BoardList />
           <ProductDelete />
+         <ProductEdit productId={1} />
         </div> */}
       </div>
     </Router>
